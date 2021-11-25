83ca63e7f92bd76b6f3aff499712edb2d0576938
marteabra <marte.abrahams1@hotmail.com>
2021-11-25 12:56:00 +0100

    mapper og gammelt innhold

---
+++ src/app/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <RouterView />
+</template>
+
+<script>
+</script>
+
+<style>
+@import "../styles/reset.css";
+@import "../styles/variables.css";
+@import "../styles/style.css";
+</style>
---
+++ src/components/ContactForm.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="contact-form">
+    <h3>Contact Us!</h3>
+    <form
+      class="contact-form__form"
+      action=""
+      ref="form"
+      @submit.prevent="sendEmail"
+    >
+      <div class="contact-form__name">
+        <!--<label>Name</label>-->
+        <input type="text" name="name" placeholder="Name" />
+      </div>
+      <div class="contact-form__email">
+        <!--<label>Email</label>-->
+        <input type="text" name="email" placeholder="Email" />
+      </div>
+      <div class="contact-form__message">
+        <!--<label>Message</label>-->
+        <input
+          class="contact-form__message--textbox"
+          type="text"
+          name="message"
+          placeholder="Your Message"
+        />
+        <input
+          class="contact-form__message--submit"
+          type="submit"
+          value="Send"
+        />
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.contact-form {
+  margin-left: 1em;
+  margin-top: 1em;
+}
+
+.contact-form__form {
+  display: grid;
+}
+
+.contact-form div {
+  padding: 10px;
+}
+
+.contact-form__message {
+  display: flex;
+  align-items: flex-end;
+}
+.contact-form__message--textbox {
+  height: 40vh;
+  width: 50vw;
+}
+</style>
