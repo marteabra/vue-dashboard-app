95265afe9c675ce5ab833c7b07acc49968dcc092
marteabra <marte.abrahams1@hotmail.com>
2021-11-25 12:59:43 +0100

    Added home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>HOMEPAGE</div>
+  <ContactForm />
+</template>
+
+<script>
+import ContactForm from "../components/ContactForm.vue";
+
+export default {
+  components: {
+    ContactForm,
+  },
+};
+
+console.log("hei");
+</script>
+
+<style>
+</style>
